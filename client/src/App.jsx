35aec183d43eb29c98e9023a3aa6a95e04807c4f
addictedtolearning <3614338+addictedtolearning@users.noreply.github.com>
2025-03-16 22:39:47 +1030

    Can now send the cause from client to serverless function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cause, setCause] = useState('');
+  const [manifesto, setManifesto] = useState('');
+
+  async function getManifesto() {
+    var resp = await fetch('/.netlify/functions/hello', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ cause }),
+    });
+    
+    if (!resp.ok) {
+      console.error('Failed to fetch manifesto:', resp.statusText);
+      return;
+    }
+
+    var data = await resp.json();
+    setManifesto(JSON.stringify(data));
+  }
 
   return (
     <>
+      <h1>
+        Viva la revolución!
+      </h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        What is the cause of your revolution?
+        <textarea
+          value={cause}
+          onChange={(e) => setCause(e.target.value)}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => getManifesto()}>
+        Get the manifesto
+      </button>
+      {manifesto && (
+        <div>
+          <h2>Manifesto:</h2>
+          <p>{manifesto}</p>
+        </div>
+      )}
     </>
   )
 }
