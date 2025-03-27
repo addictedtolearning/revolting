9ec07042835c81ffeacc1c1479f9f5aa18c26693
addictedtolearning <3614338+addictedtolearning@users.noreply.github.com>
2025-03-27 21:34:22 +1030

    Add error message (eg: when rate limit is hit)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [cause, setCause] = useState('');
   const [manifesto, setManifesto] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   async function getManifesto() {
     if (loading) return;
@@ -21,24 +22,27 @@ function App() {
         body: JSON.stringify({ cause }),
       });
 
+      setManifesto([]);
       if (!resp.ok) {
         console.error('Failed to fetch manifesto:', resp.statusText);
-        return;
+        setError(true);
+        setLoading(false);
+      } else {
+        setError(false);
+        const reader = resp.body.getReader();
+        let decoder = new TextDecoder();
+        let manifest = '';
+        while (true) {
+          const { done, value } = await reader.read();
+          if (done) break;
+          manifest += decoder.decode(value);
+          setManifesto(manifest);
+        }
       }
 
-      setManifesto([]);
-      const reader = resp.body.getReader();
-      let decoder = new TextDecoder();
-      let manifest = '';
-      while (true) {
-        const { done, value } = await reader.read();
-        if (done) break;
-        manifest += decoder.decode(value);
-        setManifesto(manifest);
-      }
     } catch (err) {
       console.error('Failed to fetch manifesto:', err);
-      alert('Failed to fetch manifesto');
+      setError(true);
     }
     setLoading(false);
   }
@@ -66,6 +70,11 @@ function App() {
           style={{ color: '#aa0000', fontSize: '36px' }}
         ></iconify-icon>
       </button>
+      {error && <div className="error">
+        <p>Sorry, we are fresh out of manifestos today!</p>
+        <p>Try again tomorrow.</p>
+        <p>You might even feel better after you sleep on it?</p>
+        </div>}
       <ManifestoRenderer manifesto={manifesto} />
     </>
   )
